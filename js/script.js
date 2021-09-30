0630145f6e510d87bf50f334e7fb119d2b934e0d
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-09-30 22:53:20 +0100

    photos loaded

--- js/script.js
+++ js/script.js
@@ -15,10 +15,41 @@ let cardsArray = [
         name: 'pizza',
         location: './images/pizza.png'
     },
+    {
+        name: 'milkshake',
+        location: './images/milkshake.png'
+    },
+    {
+        name: 'milkshake',
+        location: './images/milkshake.png'
+    },
+    {
+        name: 'ice-cream',
+        location: './images/ice-cream.png'
+    },
+    {
+        name: 'ice-cream',
+        location: './images/ice-cream.png'
+    },
+    {
+        name: 'hotdog',
+        location: './images/hotdog.png'
+    },
+    {
+        name: 'hotdog',
+        location: './images/hotdog.png'
+    },
+    {
+        name: 'fries',
+        location: './images/fries.png'
+    },
+    {
+        name: 'fries',
+        location: './images/fries.png'
+    },
 ]
 
 let grid = document.getElementById('grid');
-
 function loadCards() {
     for (let i = 0; i < cardsArray.length; i++) {
         let card = document.createElement('img');
